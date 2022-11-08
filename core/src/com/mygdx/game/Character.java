dc181f9c282d567e48d81fd58b4123b4cf4fa871
Andrei <talos.andrei08@gmail.com>
2022-11-08 16:26:42 +0100

    -Refactored/Changed Character class into a Rectangle in order to enable easy collision detection with Obstacles -Implemented Score system -Implemented general logic for the Obstacle class -Made sure that Textures that are out of the camera window are being deleted to try to avoid memory leaks

--- core/src/com/mygdx/game/Character.java
+++ core/src/com/mygdx/game/Character.java
@@ -2,122 +2,80 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
 
 public class Character {
-    public float getSpeed() {
-        return speed;
-    }
 
     private final float speed;
-
-
     private MainGame game;
     Texture charImage;
-
-    public void setxPos(float xPos) {
-        this.xPos = xPos;
-    }
-
-    private float xPos;
-
-    public void setyPos(float yPos) {
-        this.yPos = yPos;
-    }
-
-    private float yPos;
-    private float height;
-    private float width;
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getState() {
-        return state;
-    }
-
+    private Rectangle player;
     private String state = "Grounded";
-    private float jumpHeight = 25;
+
+
+    private float jumpHeight = 20;
 
 
     public Character(int speed, MainGame game) {
         this.speed = speed;
         this.game = game;
         charImage = new Texture(Gdx.files.internal("character_0006.png"));
-        initPlayer();
+        player = new Rectangle(50, game.getTileHeight(), 50, 70);
 
-    }
-
-
-    public void initPlayer() {
-        this.xPos = 50;
-        this.yPos = game.getTileHeight();
-        this.height = 70;
-        this.width = 50;
-
-    }
-
-    public void movePlayer() {
-        xPos += 4;
-    }
-
-    public float getxPos() {
-        return xPos;
-    }
-
-    public float getyPos() {
-        return yPos;
-    }
-
-    public Texture getCharImage() {
-        return charImage;
-    }
-
-    public float getHeight() {
-        return height;
-    }
-
-    public float getWidth() {
-        return width;
     }
 
 
     public void checkState() {
         switch (state) {
             case "Grounded":
-                this.xPos += speed * Gdx.graphics.getDeltaTime();
+                player.x += speed * Gdx.graphics.getDeltaTime();
                 break;
 
             case "Ascending":
                 if (jumpHeight == 0) {
                     state = "Descending";
-                    this.xPos += speed * Gdx.graphics.getDeltaTime();
-                    System.out.println(jumpHeight);
+                    player.x += speed * Gdx.graphics.getDeltaTime();
                     break;
                 }
-                this.xPos += speed * Gdx.graphics.getDeltaTime();
-                this.yPos += jumpHeight;
+                player.x += speed * Gdx.graphics.getDeltaTime();
+                player.y += jumpHeight;
                 jumpHeight -= 1;
 
                 break;
 
             case "Descending":
-                if (jumpHeight == 25) {
+                if (jumpHeight == 20) {
                     state = "Grounded";
-                    this.xPos += speed * Gdx.graphics.getDeltaTime();
-                    this.yPos -= jumpHeight;
-                    System.out.println(jumpHeight);
+                    player.x += speed * Gdx.graphics.getDeltaTime();
+                    player.y -= jumpHeight;
                     break;
                 }
-                this.xPos += speed * Gdx.graphics.getDeltaTime();
-                this.yPos -= jumpHeight;
+                player.x += speed * Gdx.graphics.getDeltaTime();
+                player.y -= jumpHeight;
                 jumpHeight += 1;
 
                 break;
-
-
         }
+    }
+
+    public float getSpeed() {
+        return speed;
+    }
+
+    public Rectangle getPlayer() {
+        return player;
+    }
 
 
+    public Texture getCharImage() {
+        return charImage;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
     }
 }
--- core/src/com/mygdx/game/MainGame.java
+++ core/src/com/mygdx/game/MainGame.java
@@ -5,8 +5,10 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.TimeUtils;
 
 import java.util.ArrayList;
 
@@ -18,13 +20,29 @@ public class MainGame implements Screen {
 
     private Platformer platformer;
     private Texture tileImage;
-    private Texture characterImage;
-    private OrthographicCamera camera;
-    private Character player;
 
+    public OrthographicCamera getCamera() {
+        return camera;
+    }
+
+    private OrthographicCamera camera;
+
+    public Character getPlayer() {
+        return player;
+    }
+
+    private Character player;
+    private Obstacle obstacle;
 
     private int tileWidth;
     private int tileHeight;
+    private BitmapFont font = new BitmapFont();
+
+
+    public ArrayList<Rectangle> getTilesArray() {
+        return tilesArray;
+    }
+
     private ArrayList<Rectangle> tilesArray = new ArrayList<>();
 
 
@@ -33,7 +51,9 @@ public class MainGame implements Screen {
         camera = new OrthographicCamera();
         camera.setToOrtho(false, screenWidth, screenHeight);
         initTile();
-        player = new Character(100, this);
+        player = new Character(350, this);
+        obstacle = new Obstacle(this);
+        font.getData().setScale(1,1.5F);
 
 
     }
@@ -52,22 +72,15 @@ public class MainGame implements Screen {
             tilesArray.add(i, new Rectangle(i * tileWidth, tileY, tileWidth, tileHeight));
 
         }
-
-
     }
 
     public void updateTiles() {
-        if ((player.getxPos() - (tilesArray.get(0).x + getTileWidth())) > 50) {
+        if ((player.getPlayer().x - (tilesArray.get(0).x + getTileWidth())) > 50) {
             tilesArray.remove(0);
             tilesArray.add(new Rectangle(tilesArray.get(tilesArray.size() - 1).x + tileWidth, tileY, tileWidth, tileHeight));
         }
     }
 
-
-    @Override
-    public void show() {
-
-    }
 
     @Override
     public void render(float delta) {
@@ -76,19 +89,51 @@ public class MainGame implements Screen {
         camera.update();
         platformer.batch.setProjectionMatrix(camera.combined);
         updateTiles();
+        if(TimeUtils.nanoTime() - obstacle.getLastSpawnTime() > 400000000 ) obstacle.manageObstacles();
+
+
         platformer.batch.begin();
         for (Rectangle tmp : tilesArray) {
             platformer.batch.draw(tileImage, tmp.x, tmp.y, tmp.height, tmp.width);
         }
-        platformer.batch.draw(player.getCharImage(), player.getxPos(), player.getyPos(), player.getWidth(), player.getHeight());
+        platformer.batch.draw(player.getCharImage(), player.getPlayer().x, player.getPlayer().y, player.getPlayer().width, player.getPlayer().height);
+        for(Rectangle tmp: obstacle.getObstacles()){
+            platformer.batch.draw(obstacle.getObstacleImage(), tmp.x,tmp.y,tmp.width,tmp.height);
+        }
+        font.draw(platformer.batch, "Score: " + obstacle.getScore(), camera.position.x+500, Gdx.graphics.getHeight()-25);
 
         platformer.batch.end();
 
         player.checkState();
+        obstacle.checkCollision();
+        System.out.println(Gdx.graphics.getFramesPerSecond());
+
         if (Gdx.input.isKeyPressed(Input.Keys.UP) && player.getState() == "Grounded")
             player.setState("Ascending");
 
     }
+
+    public int getTileWidth() {
+        return tileWidth;
+    }
+
+    public int getTileHeight() {
+        return tileHeight;
+    }
+
+    @Override
+    public void dispose() {
+        tileImage.dispose();
+        player.charImage.dispose();
+
+    }
+
+
+    @Override
+    public void show() {
+
+    }
+
 
     @Override
     public void resize(int width, int height) {
@@ -110,19 +155,7 @@ public class MainGame implements Screen {
 
     }
 
-    @Override
-    public void dispose() {
-        tileImage.dispose();
-        characterImage.dispose();
-        player.charImage.dispose();
 
-    }
-
-    public int getTileWidth() {
-        return tileWidth;
-    }
-
-    public int getTileHeight() {
-        return tileHeight;
-    }
 }
+
+
